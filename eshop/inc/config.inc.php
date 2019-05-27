6f591bd4b0021df4de7130aa832d48d7f32c33cc
Sergey Vasilenko <32385430+vasilenko-s@users.noreply.github.com>
2019-05-27 14:11:03 +0300

    Update config.inc.php

--- eshop/inc/config.inc.php
+++ eshop/inc/config.inc.php
@@ -1,10 +1,10 @@
 <?php
 //Объявление констант, переменных
 // и установка соединения с сервером базы данных
-const DB_HOST="localhost";
-const DB_LOGIN="root";
-const DB_PASSWORD="1";
-const DB_NAME="eshop";
+const DB_HOST="eu-cdbr-west-02.cleardb.net";
+const DB_LOGIN="ba9546ea66f12f";
+const DB_PASSWORD="c80d8f41";
+const DB_NAME="heroku_e2cc62cd85afeeb";
 const ORDERS_LOG="orders.log";
 
 //для хранения корзины пользователя
